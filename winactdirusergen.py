26604bb6f035c44f3a9c83191b30a72ad37e5721
francoa.taffarel <francoa.taffarel@gmail.com>
2020-07-05 15:37:23 -0300

    Create winactdirusergen.py

---
+++ winactdirusergen.py
@@ -0,0 +1,34 @@
+# !/usr/bin/env python3
+
+import argparse
+arg_parser = argparse.ArgumentParser( description = "Gerador de usernames por nome com dois campos" )
+arg_parser.add_argument( "source_file" )
+arguments = arg_parser.parse_args()
+source = arguments.source_file
+
+usernames = []
+count = 0
+
+for line in open(source):
+    name = line.lower().split()
+    usernames.append(name[0])
+    usernames.append(name[0][0] + name[-1])
+    usernames.append(name[1] + name[0][0])
+    usernames.append(name[-1][0] + name[0])
+    usernames.append(name[1])
+    usernames.append(name[0]+name[1])
+    usernames.append(name[0]+'.'+name[1])
+    usernames.append(name[0][0]+'.'+name[1]) 
+    rev= (name[0]+name[1]).strip()
+    usernames.append(rev[::-1])
+    count += 1
+
+for entry in usernames:
+    print(entry)
+print()
+print("Lista de usuários criada em users.txt") 
+print()
+with open('users.txt', 'w') as f:
+    for entry in usernames:
+        f.write("%s\n" % entry)
+  
